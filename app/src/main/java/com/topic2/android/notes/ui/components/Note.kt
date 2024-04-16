fa544d598fd4b192cd77e311c4c43ce98f4d39f5
Kashtanka13 <mitkinalera2003.80@gmail.com>
2024-04-16 15:02:53 +0300

    Rendering NoteModel

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.topic2.android.notes.theme.rwGreen
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,8 +17,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-
-@Composable fun Note(){
+import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.util.fromHex
+@Composable fun Note(
+    note: NoteModel,
+    onNoteClick: (NoteModel) -> Unit = {},
+    onNoteCheckedChange: (NoteModel) -> Unit = {}
+) {
     val backgroundShape: Shape = RoundedCornerShape(4.dp)
     Row(
         modifier = Modifier
@@ -31,7 +35,8 @@ import androidx.compose.ui.unit.sp
     ) {      NoteColor(
         modifier = Modifier
             .align(Alignment.CenterVertically)
-            .padding(start = 16.dp, end = 16.dp),      color = rwGreen,
+            .padding(start = 16.dp, end = 16.dp),
+            color = Color.fromHex(note.color.hex),
             size = 40.dp,
             padding = 4.dp,
             border = 1.dp
@@ -40,8 +45,7 @@ import androidx.compose.ui.unit.sp
             .weight(1f)
             .align(Alignment.CenterVertically)
         ) {
-            Text(
-                text = "Заголовок",
+            Text(text = note.title,
                 color = Color.Black,
                 maxLines = 1,
                 style = TextStyle(
@@ -51,7 +55,7 @@ import androidx.compose.ui.unit.sp
                 )
             )
             Text(
-                text = "Содержание",
+                text = note.content,
                 color = Color.Black.copy(alpha = 0.75f),
                 maxLines = 1,
                 style = TextStyle(
@@ -61,11 +65,12 @@ import androidx.compose.ui.unit.sp
                 )
             )
         }
+        if (note.isCheckedOff !=null)
         Checkbox(
-            checked = false,
-            onCheckedChange = { },
+            checked = note.isCheckedOff,
+            onCheckedChange = {},
             modifier = Modifier
-                .padding(start = 8.dp)
+                .padding(16.dp)
                 .align(Alignment.CenterVertically)
         )
     }
@@ -73,5 +78,5 @@ import androidx.compose.ui.unit.sp
 }
 @Preview
 @Composable
-private fun NotePreview(){ Note()
+private fun NotePreview(){ Note(note = NoteModel(1, "Заметка 1", "Содержание", null))
 }
