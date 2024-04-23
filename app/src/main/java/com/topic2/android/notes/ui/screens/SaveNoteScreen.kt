11fa2c4b5d285b616cd5bbb7c28c680926b5714a
Kashtanka13 <mitkinalera2003.80@gmail.com>
2024-04-23 15:14:14 +0300

    Picked Color

--- app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
@@ -191,6 +191,26 @@ private fun SaveNoteTopAppBar(
         }
     )
 }
+@Composable
+private fun PickedColor(color: ColorModel){
+    Row (
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = "Picked color", modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        NoteColor(
+            color = Color.fromHex(color.hex),
+            size = 40.dp,
+            border = 1.dp,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
 @Preview
 @Composable
 fun SaveNoteTopAppBarPreview(){
@@ -201,4 +221,9 @@ fun SaveNoteTopAppBarPreview(){
         onOpenColorPickerClick = {},
         onDeleteNoteClick = {}
     )
+}
+@Preview
+@Composable
+fun PickedColorPreview() {
+    PickedColor(ColorModel.DEFAULT)
 }
