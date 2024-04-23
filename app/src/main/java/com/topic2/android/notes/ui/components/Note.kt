5dda03d21d10f301fc1ab669ddafd22fd97cd305
Kashtanka13 <mitkinalera2003.80@gmail.com>
2024-04-23 17:18:59 +0300

    Edit Note, add darkTheme

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -1,17 +1,20 @@
 package com.topic2.android.notes.ui.components
-
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.Text
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
@@ -19,64 +22,69 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.domain.model.NoteModel
 import com.topic2.android.notes.util.fromHex
-@Composable fun Note(
-    note: NoteModel,
-    onNoteClick: (NoteModel) -> Unit = {},
-    onNoteCheckedChange: (NoteModel) -> Unit = {}
+
+ @OptIn(ExperimentalMaterialApi::class)
+ @Composable
+ fun Note(
+modifier: Modifier = Modifier,
+note: NoteModel,
+onNoteClick: (NoteModel) -> Unit = {},
+onNoteCheckedChange: (NoteModel) -> Unit = {},
+isSelected: Boolean = false
 ) {
-    val backgroundShape: Shape = RoundedCornerShape(4.dp)
-    Row(
+     val background = if (isSelected)
+        Color.LightGray
+    else
+        MaterialTheme.colors.surface
+
+    val modifier = null
+    Card(
+        shape = RoundedCornerShape(4.dp),
         modifier = Modifier
-        .padding(8.dp)
-        .shadow(1.dp, backgroundShape)
-        .fillMaxWidth()
-        .heightIn(min = 64.dp)
-        .background(Color.White, backgroundShape)
-    ) {      NoteColor(
-        modifier = Modifier
-            .align(Alignment.CenterVertically)
-            .padding(start = 16.dp, end = 16.dp),
-            color = Color.fromHex(note.color.hex),
-            size = 40.dp,
-            padding = 4.dp,
-            border = 1.dp
-        )
-        Column(modifier = Modifier
-            .weight(1f)
-            .align(Alignment.CenterVertically)
-        ) {
-            Text(text = note.title,
-                color = Color.Black,
-                maxLines = 1,
-                style = TextStyle(
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 16.sp,
-                    letterSpacing = 0.15.sp
-                )
-            )
-            Text(
-                text = note.content,
-                color = Color.Black.copy(alpha = 0.75f),
-                maxLines = 1,
-                style = TextStyle(
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 14.sp,
-                    letterSpacing = 0.25.sp
-                )
+            .padding(8.dp)
+            .fillMaxWidth(),
+        backgroundColor = background
+    ) {
+        ListItem(
+                            text = { Text(text = note.title, maxLines = 1) },
+                    secondaryText = {
+                        Text(text = note.content, maxLines = 1)
+                    },
+                    icon = {
+                        NoteColor(
+                            color = Color.fromHex(note.color.hex),
+                            size = 40.dp,
+                            border = 1.dp
+                        )
+
+    },
+    trailing = {
+        if (note.isCheckedOff != null) {
+            Checkbox(
+                checked = note.isCheckedOff,
+                onCheckedChange = { isChecked ->
+                    val newNote = note.copy(isCheckedOff = isChecked)
+                    onNoteCheckedChange.invoke(newNote)
+                },
+                modifier = Modifier.padding(start =  8.dp)
             )
         }
-        if (note.isCheckedOff !=null)
-        Checkbox(
-            checked = note.isCheckedOff,
-            onCheckedChange = {},
-            modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.CenterVertically)
-        )
+    },
+    modifier = Modifier.clickable {
+        onNoteClick.invoke(note)
     }
-
+    )
 }
+}
+
 @Preview
 @Composable
-private fun NotePreview(){ Note(note = NoteModel(1, "Заметка 1", "Содержание", null))
+private fun NotePreview()
+{ Note(
+    note = NoteModel(
+        1,
+        "Заметка 1",
+        "Содержимое 1",
+        null)
+)
 }
